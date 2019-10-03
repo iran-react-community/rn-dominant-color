ce6bb9048f0ec30a73f56dee8d21bdcbdcc70210
matin <zadehdolatabad@gmail.com>
2019-10-03 13:33:57 +0330

    Fix bug on index js

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ import { NativeModules, Platform } from 'react-native';
 const { RNDominantColor } = NativeModules;
 
 export const getColorFromURL = (url) => {
-  Platform.select({
-    ios: () => {
-      return new Promise((resolve, reject) => {
-        RNDominantColor.getColorFromURL(url, (primary, secondary, background, detail) => {
-          resolve({
-            primary,
-            secondary,
-            background,
-            detail
-          })
+
+  if (Platform.OS == 'ios') {
+    return new Promise((resolve, reject) => {
+      RNDominantColor.getColorFromURL(url, (primary, secondary, background, detail) => {
+        resolve({
+          primary,
+          secondary,
+          background,
+          detail
         })
       })
-    },
-    android: () => {
-      return new Promise((resolve, reject) => {
-        RNDominantColor.colorsFromUrl(url, (err, colors) => {
-          if (err) {
-            reject({
-              primary: '#00000000',
-              secondary: '#00000000',
-              background: '#00000000',
-              detail: '#00000000'
-            })
-          } else {
-            resolve({
-              primary: colors.dominantColor,
-              secondary: colors.averageColor,
-              background: colors.lightVibrantColor,
-              detail: colors.vibrantColor
-            })
-          }
-        });
-      })
-    }
-  })
+    })
+  }
+
+  if (Platform.OS == 'android') {
+    return new Promise((resolve, reject) => {
+      RNDominantColor.colorsFromUrl(url, (err, colors) => {
+        if (err) {
+          reject({
+            primary: '#00000000',
+            secondary: '#00000000',
+            background: '#00000000',
+            detail: '#00000000'
+          })
+        } else {
+          resolve({
+            primary: colors.dominantColor,
+            secondary: colors.averageColor,
+            background: colors.lightVibrantColor,
+            detail: colors.vibrantColor
+          })
+        }
+      });
+    })
+  }
 }
